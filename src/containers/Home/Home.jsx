41b37f0e29d9f2f10370dba91f31b7dcac28bab2
panhuajian <18715116489@163.com>
2018-09-04 16:51:12 +0800

    update

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './home.scss'
+
+export default class Home extends Component {
+  constructor () {
+    super()
+  }
+  render () {
+    return (
+      <div className="home">
+        我是首页
+      </div>
+    )
+  }
+}
