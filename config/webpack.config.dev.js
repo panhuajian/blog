5fa57514ab3109a1357f52b950bd4ec5771ff7ca
panhuajian <18715116489@163.com>
2018-09-04 15:29:50 +0800

    update

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -198,11 +198,15 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
           },
         ],
       },
