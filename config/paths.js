263d704c5413a5cbfa5248215b981280f92ba4e8
panhuajian <18715116489@163.com>
2018-09-07 16:15:17 +0800

    update

--- config/paths.js
+++ config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : './blog');
   return ensureSlash(servedUrl, true);
 }
 
